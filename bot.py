45ff60a76421a37afb4f1403cf7529a73e502ea8
yash3001 <yashchauhan197@gmail.com>
2020-09-27 12:58:33 +0530

    minor changes

--- bot.py
+++ bot.py
@@ -26,14 +26,18 @@ async def _8ball(ctx, *, question):
     await ctx.send(f"Question: {question}\nAnswer: {random.choice(answers)}")
 
 @client.command()
-async def insult(ctx, *, name):
-    curses_0 = "Chutiya.Jhaat ka baal.Lowde jesi shakal ka.Madarchod.Gaand ka dhakan.Kamina.Tharki.Behen ka lauda.Chut ka maindak.Hijra.Choot ka baal.Chullu bhar muth mein doob mar.Bhen ka taka.Kutte ka tatta.Gand Ka Khatmal.Tatto ke saudagar".split(".")
-    curses_1 = "gaandu.bhosdike.lodu.suwar ki aulad.randi baj.sust lund ki padaish.mera muh me le.betichod.choot ka bhoot.choot ka pakoda.apni gaand mein muthi daalta hai.apna lund choosta hai.teri gaand main kute ka lund.tu kutte ke poot, teri maa ki choot".split(".")
-    random_choice = random.choice([0,1])
-    if random_choice:
-        await ctx.send(f"{name} {random.choice(curses_1)}")
+async def insult(ctx, *, name = " "):
+    if name == " ":
+        await ctx.send("Please provide a name as well")
+
     else:
-        await ctx.send(f"{random.choice(curses_0)} {name}")
+        curses_0 = "Chutiya.Jhaat ka baal.Lowde jesi shakal ka.Madarchod.Gaand ka dhakan.Kamina.Tharki.Behen ka lauda.Chut ka maindak.Hijra.Choot ka baal.Chullu bhar muth mein doob mar.Bhen ka taka.Kutte ka tatta.Gand Ka Khatmal.Tatto ke saudagar".split(".")
+        curses_1 = "gaandu.bhosdike.lodu.suwar ki aulad.randi baj.sust lund ki padaish.mera muh me le.betichod.choot ka bhoot.choot ka pakoda.apni gaand mein muthi daalta hai.apna lund choosta hai.teri gaand main kute ka lund.tu kutte ke poot, teri maa ki choot".split(".")
+        random_choice = random.choice([0,1])
+        if random_choice:
+            await ctx.send(f"{name} {random.choice(curses_1)}")
+        else:
+            await ctx.send(f"{random.choice(curses_0)} {name}")
 
 @client.command()
 async def clear(ctx, amount=5):
